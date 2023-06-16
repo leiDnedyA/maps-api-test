70a1346b5c6acaa2ec0e04e454f6b47c2c862b2c
Ayden Diel <aydendiel@gmail.com>
2023-06-16 13:24:41 -0400

    set up dev environment

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+
+app.listen(PORT, ()=>{console.log(`App serving at port: ${PORT}`)});
