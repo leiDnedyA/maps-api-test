0ae9a37e5a82507e1b76cf3ebdbbcfc0f6f484f0
Ayden Diel <aydendiel@gmail.com>
2023-06-21 16:09:50 -0400

    Found a nice way of implementing leaflet routing machine

--- public/main.js
+++ public/main.js
@@ -1,3 +1,10 @@
+/**
+ * Refer to leaflet routing machine API here:
+ * 
+ * http://www.liedman.net/leaflet-routing-machine/api/
+ * 
+ */
+
 var map = L.map('map').setView([41.9696, -71.3565], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -5,9 +12,22 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-L.Routing.control({
-    waypoints: [
-        L.latLng(41.98285088332745, -71.36393262052391),
-        L.latLng(41.98626463081817, -71.33170362108166)
-    ]
-}).addTo(map);
+const waypoints = [
+    L.latLng(41.98285088332745, -71.36393262052391),
+    L.latLng(41.98626463081817, -71.33170362108166)
+]
+
+const control = L.Routing.control({
+    waypoints: waypoints,
+    routeWhileDragging: false,
+    draggableWaypoints: false,
+    addWaypoints: false,
+    autoRoute: false
+});
+
+control.route();
+
+control.addTo(map);
+control.hide();
+
+control._hookEvents
