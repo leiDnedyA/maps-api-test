a435b1c9787dde3de2b5ab7fba71cb4c6e083761
Ayden Diel <aydendiel@gmail.com>
2023-06-22 15:07:12 -0400

    Implemented thrift search by radius with Geoapify API

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
-const {nearbyByCategory } = require('./api/placesAPI.js');
+const { thriftsWithinRadius } = require('./api/placesAPI.js');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 
-nearbyByCategory();
+thriftsWithinRadius([41.988721379924996, -71.33801148811348], 20000)
+    .then(result => {
+        console.log(result);
+    });
 
-app.use('/api/', (req, res)=>{
+app.use('/api/', (req, res) => {
     console.log(req.params);
     res.send("Hello world");
 });
 
-app.listen(PORT, ()=>{console.log(`App serving at port: ${PORT}`)});
+app.listen(PORT, () => { console.log(`App serving at port: ${PORT}`) });
